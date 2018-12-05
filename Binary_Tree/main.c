c5c72248eb2e7964baf1b6776736833820b472ad
Dimitris Papazoglou <dmpapazog@gmail.com>
2018-12-05 13:00:05 +0200

    removed prints

--- Binary_Tree/main.c
+++ Binary_Tree/main.c
@@ -10,26 +10,21 @@ int main(int argc, char const *argv[])
     TREE_PTR root;
 
     TREE_init(&root);
-    printf("initialized.\n");
 
     TREE_insert_root(&root, 10);
-    printf("What the fuck is happening???\n");
-        
+            
     TREE_insert_left(root, 6);
-    printf("What the fuck is happening 2nd time???\n");
     TREE_insert_right(root, 14);    
-    printf("What the fuck is happening 3nd time???\n");
 
     TREE_insert_left(root->left, 5);
-    printf("What the fuck is happening 4nd time???\n");
     TREE_insert_right(root->left, 7);
-    printf("What the fuck is happening 5nd time???\n");       
+
     TREE_insert_right(root->right, 19);
-    printf("What the fuck is happening 6nd time???\n");        
+
     TREE_insert_left(root->left->left, 2);
-    printf("What the fuck is happening 7nd time???\n");
+
     TREE_insert_right(root->left->right, 8);
-    printf("What the fuck is happening 8nd time???\n");
+
     TREE_insert_right(root->left->left->left, 3);
     
     printf("\nPREORDER: ");
